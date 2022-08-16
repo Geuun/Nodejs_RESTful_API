386b268d6ae177a3232fcafa7dcd10841977465e
KimJaeGeun <jgk9612@gmail.com>
2022-08-16 20:31:50 +0900

    refactor: Fix code structure

--- src/api.js
+++ src/api.js
@@ -1,16 +1,45 @@
 // @ts-check
 
+/** jsdoc
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/**
+ * @typedef CreatePostBody
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {CreatePostBody} */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: 'my_first_post',
+    title: 'My first post',
+    content: 'Hello!',
+  },
+  {
+    id: 'my_second_post',
+    title: 'My second post',
+    content: 'Hi!',
+  },
+];
+
 /**
  * @typedef APIResponse
  * @property {number} statusCode
- * @property {*} body
+ * @property {string | Object} body
  */
 
 /**
  * @typedef Route
  * @property {RegExp} url
  * @property {'GET' | 'POST'} method
- * @property {() => Promise<*>} callback
+ * @property {(matches: string[], body: Object | undefined) => Promise<*>} callback
  */
 
 /** @type {Route[]} */
@@ -21,28 +50,71 @@ const routes = [
     callback: async () => ({
       // TODO: implement
       statusCode: 200,
-      body: {},
+      body: posts,
     }),
   },
 
   {
     url: /^\/posts\/([a-zA-Z0-9-_]+)$/,
     method: 'GET',
-    callback: async () => ({
-      // TODO: implement
-      statusCode: 200,
-      body: {},
-    }),
+    callback: async (matches) => {
+      const postId = matches[1];
+      if (!postId) {
+        return {
+          status: 404,
+          body: 'Not found',
+        };
+      }
+
+      const post = posts.find((_post) => _post.id === postId);
+
+      if (!post) {
+        return {
+          status: 404,
+          body: 'Not found',
+        };
+      }
+
+      return {
+        statusCode: 200,
+        body: post,
+      };
+    },
   },
 
   {
     url: /^\/posts$/,
     method: 'POST',
-    callback: async () => ({
-      // TODO: implement
-      statusCode: 200,
-      body: {},
-    }),
+    callback: async (_, body) => {
+      if (!body) {
+        return {
+          statusCode: 400,
+          body: 'Ill-formed request.',
+        };
+      }
+
+      /** @type {string} */
+      /* eslint-disable-next-line prefer-destructuring */
+      const title = body.title;
+      const newPost = {
+        id: title.replace(/\s/g, '_'),
+        title,
+        content: body.content,
+      };
+
+      posts.push({
+        id: title.replace(/\s/g, '_'),
+        title,
+        content: body.content,
+      });
+
+      posts.push(newPost);
+
+      return {
+        statusCode: 200,
+        body: newPost,
+      };
+    },
   },
 ];
 
--- src/main.js
+++ src/main.js
@@ -1,85 +1,66 @@
 // @ts-check
 
+const { rejects } = require('assert');
+const { resolveCaa } = require('dns');
 const http = require('http');
-
-/** jsdoc
- * @typedef Post
- * @property {string} id
- * @property {string} title
- * @property {string} content
- */
-
-/** @type {Post[]} */
-const posts = [
-  {
-    id: 'my_first_post',
-    title: 'My first post',
-    content: 'Hello!',
-  },
-  {
-    id: 'my_second_post',
-    title: 'My second post',
-    content: 'Hi!',
-  },
-];
+const { routes } = require('./api');
 
 const server = http.createServer((req, res) => {
-  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/;
-  const postIdRegexResult =
-    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined;
+  async function main() {
+    const route = routes.find(
+      (_route) =>
+        req.url &&
+        req.method &&
+        _route.url.test(req.url) &&
+        _route.method === req.method
+    );
 
-  if (req.url === '/posts' && req.method === 'GET') {
-    const result = {
-      post: posts.map((post) => ({
-        id: post.id,
-        title: post.title,
-      })),
-      totlaCount: posts.length,
-    };
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json; encoding=utf-8');
-    res.end(JSON.stringify(result));
-  } else if (postIdRegexResult && req.method === 'GET') {
-    // GET /posts/:id
-    const postId = postIdRegexResult[1];
-    const post = posts.find((_post) => _post.id === postId);
-
-    if (post) {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json; encoding=utf-8');
-      res.end(JSON.stringify(post));
-    } else {
+    if (!req.url || !route) {
       res.statusCode = 404;
-      res.end('Post not found.');
+      res.end('Not found.');
+      return;
     }
-  } else if (req.url === '/posts' && req.method === 'POST') {
-    req.setEncoding('utf-8');
-    req.on('data', (data) => {
-      /**
-       * @typedef CreatePostBody
-       * @property {string} id
-       * @property {string} title
-       * @property {string} content
-       */
 
-      /** @type {CreatePostBody} */
-      const body = JSON.parse(data);
-      posts.push({
-        id: body.title.toLowerCase().replace(/\s/g, '_'),
-        title: body.title,
-        content: body.content,
-      });
-    });
-    res.statusCode = 200;
-    res.end('Creating post');
-  } else {
-    res.statusCode = 404;
-    res.end('Not found.');
+    const regexResult = route.url.exec(req.url);
+
+    if (!regexResult) {
+      res.statusCode = 404;
+      res.end('Not found.');
+      return;
+    }
+
+    /** @type {Object.<string, *>} */
+    const reqbody =
+      (req.headers['content-type'] === 'application/json' &&
+        (await new Promise((resolve) => {
+          req.setEncoding('utf-8');
+          req.on('data', (data) => {
+            try {
+              resolve(JSON.parse(data));
+            } catch {
+              reject(new Error('Ill-formed json'));
+            }
+          });
+        }))) ||
+      undefined;
+
+    const result = await route.callback(regexResult, reqbody);
+    res.statusCode = result.statusCode;
+
+    if (typeof result.body === 'string') {
+      res.end(result.body);
+    } else {
+      res.setHeader('Content-Type', 'application/json; charset=utf8');
+      res.end(JSON.stringify(result.body));
+    }
   }
+
+  main();
 });
 
 const PORT = 4000;
 
 server.listen(PORT, () => {
+  /* eslint-disable-next-line no-console */
   console.log(`The server is listening at port: ${PORT}`);
 });
